7e1a40ccdd5ce2ff708cbc1a16df207f89227dc7
pwsd21 <pawansachdeva1998@gmail.com>
2023-11-23 17:04:12 +0530

    Youtube Clone Project setup using create-react-app

---
+++ my-youtube/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-3xl">Ram Ram Bhai Saareya Ne</h1>
+      {/* 
+       Head
+       Body
+        SideBar
+         MenuItems
+        MainContainer
+         ButtonList
+         VideoContainer
+           VideoCard
+       */}
+    </div>
+  );
+}
+
+export default App;
