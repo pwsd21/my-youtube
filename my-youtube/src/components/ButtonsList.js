f8ccaabf58823c97070c0660be77b1edfa179beb
pwsd21 <pawansachdeva1998@gmail.com>
2023-11-23 22:59:40 +0530

    Added ButtonList Feature

--- my-youtube/src/components/ButtonsList.js
+++ my-youtube/src/components/ButtonsList.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Button from "./Button";
+import { list } from "../utils/constants";
 
 const ButtonsList = () => {
-  return <div>ButtonsList</div>;
+  return (
+    <div className="flex">
+      {list?.map((item) => (
+        <Button name={item} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonsList;
